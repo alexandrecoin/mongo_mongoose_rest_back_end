77d7b724d73d3e0bea858ce07e5c87e0712d21ce
Alexandre Coin <alexandre.pc.coin@gmail.com>
2019-09-29 09:26:47 +0200

    build(client): transfer web-app to client folder

---
+++ client/src/components/MealCategories.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import MealsByCategory from './MealsByCategory';
+import { Route } from 'react-router-dom';
+
+const MealCategories = () => {
+  const [categories, setCategories] = useState([]);
+  const [url, setUrl] = useState('');
+
+  async function fetchApiData() {
+    const response = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/categories.php',
+    );
+    const data = await response.json();
+    setCategories(data.categories);
+  }
+  useEffect(() => {
+    fetchApiData();
+  }, []);
+
+  async function handleClick(e, data) {
+    e.preventDefault();
+    setUrl(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${data.strCategory}`,
+    );
+  }
+
+  return (
+    <Fragment>
+      <ul>
+        {categories.map((category) => (
+          <button
+            key={category.idCategory}
+            onClick={(e) => handleClick(e, category)}
+          >
+            {category.strCategory}
+          </button>
+        ))}
+      </ul>
+      {url.length > 0 ? (
+        <Route
+          path="/mealsByCategory"
+          render={(props) => <MealsByCategory {...props} url={url} />}
+        />
+      ) : null}
+    </Fragment>
+  );
+};
+
+export default MealCategories;
