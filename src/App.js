a85523c3563ea2f8ea6c4f50b530340e7c27dd7b
Alexandre Coin <alexandre.pc.coin@gmail.com>
2019-09-29 07:36:58 +0200

    setup(client): add react-router and setup in app.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import MealCategories from './components/MealCategories';
 import Layout from './layout';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Layout />
-      <MealCategories />
-    </div>
+    <Router>
+      <div className="App">
+        <Layout />
+        <Route exact path="/" component={MealCategories} />
+      </div>
+    </Router>
   );
 }
 
