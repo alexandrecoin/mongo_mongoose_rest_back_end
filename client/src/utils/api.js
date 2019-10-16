4007c4fed3fa02a779e5a73ed6eb676a7d4f1dfa
Alexandre Coin <alexandre.pc.coin@gmail.com>
2019-10-16 21:50:55 +0200

    refacto(front): remove old client folder + add material

--- client/src/utils/api.js
+++
@@ -1,5 +0,0 @@
-const categoriesUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
-const mealByCategoryUrl =
-  'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
-
-export { categoriesUrl, mealByCategoryUrl };
