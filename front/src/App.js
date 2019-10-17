99e07b7cce1264897654a86a33376121890d7233
Alexandre Coin <alexandre.pc.coin@gmail.com>
2019-10-17 06:52:57 +0200

    build(front): add react-router

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Login from './components/Login';
 import Meals from './components/Meals';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Login />
-        <Meals />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Route path="/login" component={Login} />
+          <Route exact path="/meals" component={Meals} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
