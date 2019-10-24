da70cdde1313ba49b05d34357298464573da3639
Alexandre Coin <alexandre.pc.coin@gmail.com>
2019-10-24 21:37:12 +0200

    design(front): login page WIP

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import TextField from '@material-ui/core/TextField';
+import styled from 'styled-components';
 import axios from 'axios';
 
 export default function Login(props) {
@@ -15,33 +20,67 @@ export default function Login(props) {
       email,
       password,
     });
-    if (login.data.token) console.log(login.data)
+    if (login.data.token) console.log(login.data);
   }
 
   return (
-    <div className="Login">
-      <form onSubmit={handleSubmit}>
-        <div id="email">
-          <label>Email</label>
-          <input
-            autoFocus
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div id="password">
-          <label>Password</label>
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type="password"
-          />
-        </div>
-        <button disabled={!validateForm()} type="submit">
-          Login
-        </button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <StyledCard className="loginCard">
+        <StyledTextField
+          id="outlined-email-input"
+          label="Email"
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <StyledTextField
+          id="outlined-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <StyledCardActions>
+          <Button disabled={!validateForm()} type="submit">
+            Login
+          </Button>
+        </StyledCardActions>
+      </StyledCard>
+    </form>
   );
 }
+
+const StyledCard = styled(Card)`
+  max-height: 30%;
+  max-width: 25%;
+  font-size: 1.5em;
+  text-align: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const StyledTextField = styled(TextField)`
+  width: '100%'
+  font-size: 1.5em;
+  text-align: center;
+`;
+
+const StyledCardActions = styled(CardActions)`
+  background: linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%);
+  border-radius: 10px;
+  color: white;
+  text-align: center;
+  margin: auto;
+  width: 25%;
+  padding: 0 30px;
+  box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+`;
